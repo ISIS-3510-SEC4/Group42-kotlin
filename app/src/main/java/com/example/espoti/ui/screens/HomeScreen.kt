c4c7037015c06de7a17037632e01fc124051751b
CamMV <j.murciav@uniandes.edu.co>
2026-09-18 13:54:56 -0500

    Fonts, colors and general update

--- app/src/main/java/com/example/espoti/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/espoti/ui/screens/HomeScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.shape.CircleShape
@@ -146,9 +147,11 @@ private fun HomeHeader() {
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.Top
     ) {
+        var isDarkTheme = isSystemInDarkTheme()
+        var iconResource = if (isDarkTheme) R.drawable.logoicon else R.drawable.logoicondark
         Row(verticalAlignment = Alignment.CenterVertically) {
             Image(
-                painter = painterResource(R.drawable.logoicon),
+                painter = painterResource(id = iconResource),
                 contentDescription = "Espoti logo",
                 modifier = Modifier
                     .size(40.dp)
--- app/src/main/java/com/example/espoti/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/espoti/ui/screens/LoginScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -68,6 +69,9 @@ fun LoginScreen(
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
 
+    var isDarkTheme = isSystemInDarkTheme()
+    var iconResource = if (isDarkTheme) R.drawable.logoicon else R.drawable.logoicondark
+
     var emailError by remember { mutableStateOf(false) }
     var passwordError by remember { mutableStateOf(false) }
 
@@ -116,7 +120,7 @@ fun LoginScreen(
         Spacer(modifier = Modifier.height(40.dp))
 
         Image(
-            painter = painterResource(R.drawable.logoicon),
+            painter = painterResource(id=iconResource),
             contentDescription = "Espoti logo",
             modifier = Modifier.size(110.dp)
         )
